1c0505e550841a3339797fef9c1335aace85d06a
kirigaya07 <crimsomemoon7@gmail.com>
2024-07-28 16:32:06 +0530

    Backend Created

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+import { log } from "console";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
