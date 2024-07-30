9abfcf8d4c8bec6b7c37809c2b244a6fd076a525
kirigaya07 <crimsomemoon7@gmail.com>
2024-07-30 15:01:48 +0530

    Connected to the database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,17 @@
-import { log } from "console";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("MongoDb connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 const port = 3000;
