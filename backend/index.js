96723c68d718a110c57d85a4df6e9187bf7a0e26
kirigaya07 <crimsomemoon7@gmail.com>
2024-07-31 13:48:25 +0530

    Api route created

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user/user.js";
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ mongoose
 
 const app = express();
 const port = 3000;
+
+app.use("/api/user", userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
