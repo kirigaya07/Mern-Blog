7ade8b3548bac6589cd66f5c29fb8c093815b997
kirigaya07 <crimsomemoon7@gmail.com>
2024-08-08 23:29:54 +0530

    Created signin api route

--- backend/contollers/auth/auth.js
+++ backend/contollers/auth/auth.js
@@ -1,6 +1,7 @@
 import User from "../../models/user/user.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../../utils/errors.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -23,4 +24,32 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid Password"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
+  } catch (error) {}
 };
--- backend/routes/auth/auth.js
+++ backend/routes/auth/auth.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { signup } from "../../contollers/auth/auth.js";
+import { signin } from "../../contollers/auth/auth.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
