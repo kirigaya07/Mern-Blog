eda1beeed6415478726ba64613a124d91e3b533d
kirigaya07 <crimsomemoon7@gmail.com>
2024-08-13 17:10:08 +0530

    Added delete api route

--- backend/routes/user/user.js
+++ backend/routes/user/user.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { test, updateUser } from "../../contollers/user/user.js";
+import { deleteUser, test, updateUser } from "../../contollers/user/user.js";
 import { verifyToken } from "../../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser);
 
 export default router;
