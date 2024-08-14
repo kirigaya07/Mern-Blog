b922cbba7e223ca56d4b7fa434ab767cf01cdff7
kirigaya07 <crimsomemoon7@gmail.com>
2024-08-14 23:14:19 +0530

    Completed create post ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,8 @@ import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import FooterCom from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
+import CreatePost from "./pages/CreatePost";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 
 function App() {
   return (
@@ -21,6 +23,10 @@ function App() {
         <Route element={<PrivateRoute />}>
           {" "}
           <Route path="/dashboard" element={<Dashboard />} />{" "}
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          {" "}
+          <Route path="/create-post" element={<CreatePost />} />{" "}
         </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
--- frontend/src/components/DashProfile.jsx
+++ frontend/src/components/DashProfile.jsx
@@ -22,9 +22,10 @@ import {
   deleteUserFailure,
   signOutSuccess,
 } from "../redux/user/userSlice";
+import { Link } from "react-router-dom";
 
 export default function DashProfile() {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -223,9 +224,25 @@ export default function DashProfile() {
           placeholder="password"
           onChange={handleChange}
         />
-        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
-          Update
+        <Button
+          type="submit"
+          gradientDuoTone="purpleToBlue"
+          outline
+          disabled={loading && imageFileUploading}
+        >
+          {loading ? "Loading..." : "Update"}
         </Button>
+        {currentUser && (
+          <Link to={"/create-post"}>
+            <Button
+              type="button"
+              gradientDuoTone="purpleToBlue"
+              className="w-full"
+            >
+              Create a Post
+            </Button>
+          </Link>
+        )}
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span onClick={() => setShowModal(true)} className="cursor-pointer">
