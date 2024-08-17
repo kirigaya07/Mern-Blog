3164b7e5be7eb8421dba8c14594be3c95c18996c
kirigaya07 <crimsomemoon7@gmail.com>
2024-08-17 17:59:56 +0530

    added delete post functionality

--- backend/routes/post/post.js
+++ backend/routes/post/post.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { verifyToken } from "../../utils/verifyUser.js";
-import { create, getPost } from "../../contollers/post/post.js";
+import { create, deletepost, getPost } from "../../contollers/post/post.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
 router.get("/getposts", getPost);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
 
 export default router;
