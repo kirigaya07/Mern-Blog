e131b774f6c8c0f82567e38d6f6ec4cf3b2434ff
kirigaya07 <crimsomemoon7@gmail.com>
2024-08-20 14:47:13 +0530

    Minor updates

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
