c242f0b54da48ddbc46c5303b4da6d27d219881f
kirigaya07 <crimsomemoon7@gmail.com>
2024-08-28 22:43:08 +0530

    completed about and project pages

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,48 @@
+/* eslint-disable react/no-unescaped-entities */
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Zudo's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to my blog, a platform dedicated to sharing insights,
+              experiences, and knowledge in the world of web development and
+              technology. Here, I explore a wide range of topics through
+              in-depth articles that offer valuable insights and practical
+              advice for developers at all levels. Whether you're just starting
+              out or looking to deepen your expertise, the content here is
+              designed to help you navigate the complexities of web development
+              with confidence.
+            </p>
+            <p>
+              In addition to articles, this blog serves as a showcase for my
+              projects, offering a behind-the-scenes look at the work I do. I
+              provide detailed explanations and key takeaways from each project,
+              giving you an opportunity to learn from real-world examples.
+              You'll also find step-by-step guides that are crafted to assist
+              you in building and enhancing web applications, covering
+              everything from basic concepts to more advanced techniques.
+            </p>
+            <p>
+              Beyond technical content, this blog features thought pieces that
+              reflect on current trends, challenges, and opportunities within
+              the tech industry. My goal is to create a space where ideas can be
+              shared and discussed, fostering a community of learners and
+              innovators. Whether you're here to gain new knowledge, draw
+              inspiration from my projects, or stay informed about the latest
+              industry trends, this blog is a resource designed to support your
+              journey in the digital world. Thank you for being part of this
+              community.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,13 +1,13 @@
-"use client";
+import CallToAction from "../components/CallToAction";
 
-import { Button } from "flowbite-react";
-
-function Projects() {
+export default function Projects() {
   return (
-    <Button color="blue" pill>
-      Blue
-    </Button>
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        Build fun and engaging projects while learning HTML, CSS and JavaScript!
+      </p>
+      <CallToAction />
+    </div>
   );
 }
-
-export default Projects;
