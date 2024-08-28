3138a5282e19cf85014353a1e48cac49e7dc4e98
kirigaya07 <crimsomemoon7@gmail.com>
2024-08-28 23:56:17 +0530

    added pre installation

--- api/index.js
+++ api/index.js
@@ -34,10 +34,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 app.use((err, req, res, next) => {
